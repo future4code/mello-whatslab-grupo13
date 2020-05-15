ddb987e81afe6286db41e2599dfd7d60c6782e9a
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2020-05-15 13:43:58 -0300

    parte obrigatoria

--- whats/src/App.js
+++ whats/src/App.js
@@ -5,15 +5,35 @@ import NomeUsuario from './components/NomeUsuario/NomeUsuario';
 import Mensagem from './components/Mensagem/Mensagem';
 import Enviar from './components/Enviar/Enviar';
 import Conversa from './components/Conversa/Conversa';
+import styled from 'styled-components';
+
+const Principal = styled.div`
+  display: flex;
+  /* justify-content: ce; */
+  border: solid 1px black;
+  height: 900px;
+  width: 500px;
+  flex-direction: column-reverse;
+  /* justify-content: space-around; */
+`
+
+const Elementos = styled.div`
+  display: flex;
+  justify-content: center;
+`
 
 function App() {
   return (
-    <div>
-      <Conversa />
-      <NomeUsuario />
-      <Mensagem />
-      <Enviar />
-    </div>
+    <Principal>
+      <div>
+        <Conversa />
+        <Elementos>
+          <NomeUsuario />
+          <Mensagem />
+          <Enviar />
+        </Elementos>
+      </div>
+    </Principal>
   );
 }
 
